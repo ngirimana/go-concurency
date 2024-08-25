c42f528f6d8960c79977584a62898951e27c84e3
ngirimana <chadrackngirimana@gmail.com>
2024-08-25 18:53:23 +0200

    go toutines and channels

--- main.go
+++ main.go
@@ -7,20 +7,36 @@ import (
 
 func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
+
+	// dones := make([]chan bool, 4)
 	done := make(chan bool)
+
+	// for i := 0; i < 4; i++ {
+	// 	dones[i] = make(chan bool)
+	// }
+
 	go greet("Nice to meet you!", done)
 	go greet("How are you?", done)
 
 	go slowGreet("How ... are ... you ...?", done)
 	go greet("I hope you're liking the course!", done)
-	<-done
+
+	// for i := 0; i < 4; i++ {
+	// 	<-dones[i]
+	// }
+
+	for range done {
+		// fmt.Println("Done", doneChan)
+	}
 }
